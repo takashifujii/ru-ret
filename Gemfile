2970045dc40b7c338bad049cf15b8e8e81bda469
Fujii Takashi <fujii@ficc.jp>
2016-05-27 14:34:51 +0900

    抽選会つくった

--- Gemfile
+++ Gemfile
@@ -12,3 +12,9 @@ gem "wdm", "~> 0.1.0", :platforms => [:mswin, :mingw]
 
 # Windows does not come with time zone data
 gem "tzinfo-data", platforms: [:mswin, :mingw, :jruby]
+
+# jquery
+gem 'jquery-middleman', :require => false
+
+# es6
+gem "middleman-es6", github: "vast/middleman-es6"
---
+++ source/javascripts/gurugurudon.js
@@ -0,0 +1,46 @@
+$(function(){
+
+  let NAMES = ["福岡 陽","藤井 隆史","安藤 勝","小林 慎平","城島 由紀子","豊嶋 七瀬","稲葉 優一郎","林 信輔","増田 有紗"];
+
+  const $text = $("#text");
+  const $button = $("#button");
+  const $list = $("#list");
+
+  let max = 8;
+  let isStop = false;
+  let interval, randomNum;
+
+  interval = setInterval(ruuuurettoppoi, 1);
+
+  $button.on('click', function(){
+    if(!isStop){
+      clearInterval(interval);
+
+      list_ni_sonyu(NAMES[randomNum]);
+      NAMES.splice(randomNum, 1);
+      max--;
+    } else {
+      interval = setInterval(ruuuurettoppoi, 1);
+    }
+    isStop = !isStop;
+  });
+
+  function list_ni_sonyu(name){
+    var list = $("<li>" + name + "</li>");
+
+    $list.append(list);
+  }
+
+  function ruuuurettoppoi(){
+    randomNum = Math.round(Math.random() * max);
+    const randomName = NAMES[randomNum];
+
+    if(randomName){
+      $text.text(randomName);
+    } else {
+      $text.text("抽選 終わり");
+      $button.text("押せないよ！").css({"background-color": "#777"}).removeClass('active').off('click');
+    }
+
+  }
+});
